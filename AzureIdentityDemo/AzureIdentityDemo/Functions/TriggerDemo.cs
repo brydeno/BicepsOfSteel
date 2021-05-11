040536454998006360e31e490e2eacdcba724746
Bryden Oliver <77371877+brydeno@users.noreply.github.com>
2021-05-11 13:46:20 +1000

    And a trigger

--- AzureIdentityDemo/AzureIdentityDemo/Functions/TriggerDemo.cs
+++ AzureIdentityDemo/AzureIdentityDemo/Functions/TriggerDemo.cs
@@ -1,21 +1,56 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AzureIdentityDemo.Functions
 {
-    public static class TriggerDemo
+    public class TriggerDemo
     {
+        EventHubProducerClient _eventHubClient = null;
+        ServiceBusClient _serviceBusClient = null;
+        ServiceBusSender _serviceBusSender = null;
+        CosmosClient _cosmosClient = null;
+        public TriggerDemo(IConfiguration configuration)
+        {
+            _eventHubClient = new EventHubProducerClient(configuration["EventHub:NameSpace"], configuration["EventHub:Name"], new DefaultAzureCredential());
+            _serviceBusClient = new ServiceBusClient(configuration["ServiceBus:NameSpace"], new DefaultAzureCredential());
+            _serviceBusSender = _serviceBusClient.CreateSender(configuration["ServiceBus:Name"]);
+            _cosmosClient = new CosmosClient(configuration["Cosmos:Address"], new DefaultAzureCredential());
+        }
+
         // Note we just have the name of a queue. The Connection property is in the form of https://identitydemo.queue.core.windows.net
         // There's nothing secret in the config being loaded from your application config
-
         [FunctionName("TriggerDemo")]
-        public static void Run([QueueTrigger("%Queue:Name%", Connection = "Queue:Address")] string job, ILogger log, CancellationToken ct)
+        public async Task Run([QueueTrigger("%Queue:Name%", Connection = "Queue:Address")] string job, ILogger log, CancellationToken ct)
         {
-            log.LogInformation($"got a job {job}");
+            var data = new EventData(new BinaryData(job));
+            await _eventHubClient.SendAsync(new EventData[] { data });
         }
+
+        [FunctionName("EventHubTrigger")]
+        public async Task EventHubTrigger([EventHubTrigger("%EventHub:Name%", Connection = "EventHub:Address")] EventData[] events, ILogger log)
+        {
+            foreach (var eventdata in events)
+            {
+                ServiceBusMessage message = new ServiceBusMessage(eventdata.EventBody.ToString());
+                await _serviceBusSender.SendMessageAsync(message);
+            }
+        }
+
+        //[FunctionName("ServiceBusTrigger")]
+        //public async Task ServiceBusTrigger([ServiceBusTrigger("%ServiceBus:Name%", Connection = "ServiceBus:Address")] EventData[] events, ILogger log)
+        //{
+        //    _cosmosClient.GetContainer()
+
+        //}
+
     }
 }
